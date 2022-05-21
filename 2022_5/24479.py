ecf45bb5e37d320d0aad6327df20a09606e2054b
JeongHoon Lee <sherry0_@kakao.com>
2022-05-21 23:50:34 +0900

    20220521OC

---
+++ 2022_5/24479.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+N, M, R = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N+1)]
+visited = [False]*(N+1)
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(N):
+    graph[i].sort()
+
+res = [0] * (N+1)
+temp = 1
+
+
+def dfs(V):
+    global temp
+    res[V] = temp
+    temp += 1
+    visited[V] = True
+
+    for target in graph[V]:
+        if not visited[target]:
+            dfs(target)
+
+
+dfs(R)
+
+for i in range(1, N+1):
+    print(res[i])
